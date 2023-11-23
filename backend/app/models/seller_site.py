49070b0de7db21cdccff0a5ea4baa7c75f1d1151
yunko006 <peyrachethomas@gmail.com>
2023-11-23 21:00:31 +0100

    Add: Relationship to each models
    Fix on many to one relationship ?

--- backend/app/models/seller_site.py
+++ backend/app/models/seller_site.py
@@ -2,14 +2,16 @@ from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
 from sqlalchemy.orm import relationship
 
 from app.database import Base
-from app.models.keyboard import Keyboard
+
+# from app.models.keyboard import Keyboard
 
 
 class SellerSite(Base):
     __tablename__ = "seller_sites"
 
-    site_id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True)
     site_name = Column(String)
     site_url = Column(String)
 
+    # keyboard_id = Column(Integer, ForeignKey("keyboards.id"))
     keyboards = relationship("Keyboard", back_populates="seller_site")
