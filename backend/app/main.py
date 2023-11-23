99a20b0e6eab10751aaa106bdf3e000fbf431c0b
yunko006 <peyrachethomas@gmail.com>
2023-11-23 21:01:51 +0100

    Need refactor later

--- backend/app/main.py
+++ backend/app/main.py
@@ -5,8 +5,10 @@ from fastapi import FastAPI
 
 from app.api import ping
 
-# from app.models.keyboard import Keyboard
-# from sqlalchemy.orm import Session
+from app.models.seller_site import SellerSite
+from app.models.keyboard import Keyboard
+from app.models.description import Description
+from sqlalchemy.orm import Session
 
 from app.database import SessionLocal
 
@@ -19,14 +21,27 @@ def create_application() -> FastAPI:
     db = SessionLocal()
 
     # Test pour si ma connection est bonne.
-    # def create_kb(db: Session):
-    #     db_user = Keyboard(name="quatre", url="test", price=2)
-    #     db.add(db_user)
+    # def create_seller_site(db: Session):
+    #     kb = Keyboard(name="TEST42", url="url", price="5000", seller_site_id=4)
+    #     db.add(kb)
     #     db.commit()
-    #     db.refresh(db_user)
-    #     return db_user
+    #     db.refresh(kb)
 
-    # create_kb(db)
+    #     db_seller_site = SellerSite(
+    #         site_name="EpoMakerERGRTEHG", site_url="https://epomaker.com/"
+    #     )
+    #     db.add(db_seller_site)
+    #     db.commit()
+    #     db.refresh(db_seller_site)
+    #     return db_seller_site
+
+    #     # db_description = Description(layout="ISO", keyboard_id=3)
+    #     # db.add(db_description)
+    #     # db.commit()
+    #     # db.refresh(db_description)
+    #     # return db_description
+
+    # create_seller_site(db)
     # Fin de test.
 
     # api routes here
