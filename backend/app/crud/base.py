23178e572f01032332cd7fdac4b70a36e26c184c
yunko006 <peyrachethomas@gmail.com>
2023-11-24 18:30:06 +0100

    Delete base.py

--- backend/app/crud/base.py
+++
@@ -1,8 +0,0 @@
-from sqlalchemy.orm import Session
-
-from app.models.keyboard import Keyboard
-from app.schemas.hero_schema import *
-
-
-def get_kb(db: Session, kb_id: int):
-    return db.query(Keyboard).filter(Keyboard.id == kb_id).first()
