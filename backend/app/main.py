232ab98099ab21c1b4e918113866bb2f28f3d918
yunko006 <peyrachethomas@gmail.com>
2023-11-26 23:37:10 +0100

    Refractor: Clean main & Add: Keyboard router

--- backend/app/main.py
+++ backend/app/main.py
@@ -3,9 +3,7 @@ import os
 from fastapi import FastAPI
 
 
-from app.api import ping, seller_sites
-
-# from app.database import SessionLocal
+from app.api import ping, seller_sites, keyboards
 
 
 def create_application() -> FastAPI:
@@ -16,6 +14,9 @@ def create_application() -> FastAPI:
     application.include_router(ping.router)
     application.include_router(
         seller_sites.router, prefix="/seller_sites", tags=["seller_sites"]
+    )
+    application.include_router(
+        keyboards.router, prefix="/keyboards", tags=["keyboards"]
     )
 
     return application
