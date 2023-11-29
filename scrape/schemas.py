0549a9f7ceee985d56ade10e387b4b541fda68ae
yunko006 <peyrachethomas@gmail.com>
2023-11-29 23:25:53 +0100

    Create: Dataclass to serialize data from scraper

--- scrape/schemas.py
+++ scrape/schemas.py
@@ -19,3 +19,24 @@ class KeychronScrapeResults:
     price: float
     image: Optional[str]
     seller_site_id: int = 67  # hardcode "2" pour simplifier pour le moment.
+
+
+@dataclass
+class DescriptionScrapeResults:
+    layout: Optional[str]
+    brand: Optional[str]
+    product_type: Optional[str]
+    profile: Optional[str]
+    layout_size: Optional[str]
+    layout_standard: Optional[str]
+    layout_ergonomics: Optional[str]
+    hot_swappable: Optional[bool]
+    knob_support: Optional[bool]
+    rgb_support: Optional[bool]
+    display_support: Optional[bool]
+    qmk_via_support: Optional[bool]
+    connection: Optional[str]
+    battery_capacity: Optional[str]
+    mount_style: Optional[str]
+    case_material: Optional[str]
+    keycap_material: Optional[str]
